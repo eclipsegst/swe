e7584f7d83090ada3104480ab24e0c63cba86ddf
zztg2 <zztg2@mail.missouri.edu>
2014-04-09 00:58:58 -0500

    Added create.php to views

---
+++ Source/application/views/create.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Create</title>
+<script>
+	function validateForm()
+	{
+		var t=document.forms["newpageform"]["title"].value;
+		var d=document.forms["newpageform"]["description"].value;
+		if (t==null || t=="")
+	  	{
+	  		alert("Title must be filled out!");
+	  		return false;
+	  	}
+	  	if (d==null || d=="")
+	  	{
+	  		alert("Description must be filled out!");
+	  		return false;
+	  	}
+	}
+</script>
+</head>
+<body>
+
+<form method="POST" action="create/create_assignment" id="newpageform" onsubmit="return validateForm()">
+<table align="left">
+	<tr>
+  		<td align="left">Assignment Title:</td>
+  		<td><input type="text" name="title" size="70"></td>
+	</tr>
+	<tr>
+  		<td align="left">Description:</td>
+  		<td>
+		<textarea rows="10" cols="70" name="description" form="newpageform" placeholder="Enter content here..."></textarea>
+
+  		</td>
+	</tr>
+	<tr><td></td><td><input type="submit" name = "submit" value="Create Page" size = "20"/> | <a href="www.google.com">Cancel</a></td>
+	</tr>
+</table>
+</form>
+
+
+<?php echo $error; ?>
+
+
+</body>
+</html>
