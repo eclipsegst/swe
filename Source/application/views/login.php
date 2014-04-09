2cf34c7d3ded75df9d26600dc4a91bd29df13ad6
zztg2 <zztg2@mail.missouri.edu>
2014-04-08 21:48:55 -0500

    Updated the layout of login form.

--- Source/application/views/login.php
+++ Source/application/views/login.php
@@ -1,89 +1,22 @@
 <!DOCTYPE html>
 <!-- saved from url=(0039)http://getbootstrap.com/examples/cover/ -->
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="shortcut icon" href="http://cdn.polkadotbride.com/wp-content/uploads/2008/03/n.jpg">
-
     <title>Login</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="http://getbootstrap.com/examples/cover/cover.css" rel="stylesheet">
-    <link href="../login_files/signin.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  <style id="holderjs-style" type="text/css"></style></head>
+  <style id="holderjs-style" type="text/css"></style>
+</head>
 
   <body>
+    <h1> Login </h1>
+    <form method="POST" action="<?php echo base_url();?>login/roleCheck">
+      <table>
+        <tr><td>Pawprint: </td><td><input type="text" name="username" placeholder="Pawprint"></td></tr>
+        <tr><td>Password: </td><td><input type="password" name="password" placeholder="Password"></td></tr>
+        <tr><td><input type="submit" name="submit"></td></tr>
+      </table>
+    </form>
+  <div>  <?php echo $error; ?> </div>
 
-    <div class="site-wrapper">
-
-      <div class="site-wrapper-inner">
-
-        <div class="cover-container">
-
-          <div class="masthead clearfix">
-            <div class="inner">
-              <h3 class="masthead-brand">You Rock!</h3>
-              <ul class="nav masthead-nav">
-                <li><a href="../">Home</a></li>
-                <li><a href="../index.php/contact">Contact</a></li>
-                <li class="active"><a href="">Login</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <!-- <div class="inner cover"> -->
-<!--             <h1 class="cover-heading">Group Member</h1> -->
-             <!-- <div class="container"> -->
-
-              <form class="form-signin" role="form" method="POST" action="login/roleCheck">
-                
-                <h2 class="form-signin-heading">Please sign in</h2>
-                <input type="" class="form-control" placeholder="Pawprint" required autofocus>
-                <input type="password" class="form-control" placeholder="Password" required>
-                <label class="checkbox">
-                  <input type="checkbox" value="remember-me"> Remember me
-                </label>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-              </form>
-
-           <!-- </div> /container  -->
-
-          <!-- </div> -->
-
-          <div class="mastfoot">
-            <div class="inner">
-              <!-- <p>This project uses <a href="">Bootstrap</a>, by <a href="">someone</a>.</p> -->
-            </div>
-          </div>
-
-        </div>
-
-
-      </div>
-
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./login_files/jquery.min.js"></script>
-    <script src="./login_files/bootstrap.min.js"></script>
-    <script src="./login_files/docs.min.js"></script>
-  
-
-</body></html>
+</body>
+</html>
