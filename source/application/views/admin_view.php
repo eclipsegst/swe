14702a8c95a99844fcafced082ee1988d146223d
Zhaolong <zztg2@mail.missouri.edu>
2014-04-18 15:10:06 -0500

    Added instruction

--- source/application/views/admin_view.php
+++ source/application/views/admin_view.php
@@ -1,5 +1,11 @@
 <h1> Welcome to Admin page </h1>
-<a href="<?php echo base_url();?>user"> User Page </a><br/>
+<h3>Instruction</h3>
+<ul>
+	<li>Account page lists all the users and their roles.</li>
+	<li>You can change the role to access different pages.</li>
+
+</ul>
+<a href="<?php echo base_url();?>user"> Account Page </a><br/>
 
 <a href="<?php echo base_url();?>add_ta"> Add TA Page </a><br >
 <a href="<?php echo base_url();?>create"> Create Assignment Page </a><br >
@@ -7,9 +13,4 @@
 <a href="<?php echo base_url();?>student"> Student Page </a><br />
 <a href="<?php echo base_url();?>ta"> TA Page </a><br />
 <a href="<?php echo base_url();?>professor"> Professor Page </a><br />
-<a href="<?php echo base_url();?>course"> Add course Page </a><br />
-
-<br /> 
-If you want to access student, TA or Professor page, make sure you role correct.
-<br />
-You can change your role using User Page.
+<a href="<?php echo base_url();?>course"> Course List Page </a><br />
