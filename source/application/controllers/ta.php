dc62088ea0363359ca0871d4a292811f9a3223be
Zhaolong <zztg2@mail.missouri.edu>
2014-04-19 13:11:50 -0500

    Added course list to ta page

--- source/application/controllers/ta.php
+++ source/application/controllers/ta.php
@@ -7,6 +7,10 @@ class Ta extends CI_Controller {
 		$role = $this->session->userdata('role');
 		if($role=='ta' || $role=='admin'){
 			$data['msg'] = $msg;
+		    $this->load->model('course_model');
+		    $query = $this->course_model->get_all_courses();
+		    
+		    $data['query'] = $query->result();
 			$this->load->view('ta_view', $data);
 		}else{
 			redirect('login');
--- source/application/views/ta_view.php
+++ source/application/views/ta_view.php
@@ -1,2 +1,33 @@
 <a href="<?php echo base_url();?>login/do_logout/">Logout</a>| Welcome <?php echo $this->session->userdata('firstname');?></a>
 <h1> Welcome to TA page </h1>
+
+<form action="<?php echo base_url();?>course_new"><input type="submit" value="Added a new course"></form>
+
+<div>
+	<table style="width:1200px">
+		<tr>
+		 	<th>cid</th>
+		 	<th>courseid</th>
+		    <th>coursename</th>
+		    <th>description</th>    
+		    
+		</tr>
+		<?php 
+		foreach ($query as $course)
+		{
+		?>
+		<tr>
+			<td><?php echo $course->cid; ?></td>
+			<td><?php echo $course->courseid; ?></td>
+      		<td><?php echo $course->coursename; ?></td>
+      		<td><?php echo $course->description; ?></td>
+			<td>
+			<a href="<?php echo base_url(); ?>assignment_new?courseid=<?php echo $course->courseid; ?>">Add an assignment</a>
+			<a href="<?php echo base_url(); ?>course_single?courseid=<?php echo $course->courseid; ?>">Select</a>
+			</td>
+		<tr>
+		<?php
+		}
+		?>
+	</table>
+</div>
