9594c8e88300621a4ac15f1fbd05694634a55f84
Zhaolong <zztg2@mail.missouri.edu>
2014-04-19 16:39:16 -0500

    Added table style

--- source/assets/template/header.php
+++ source/assets/template/header.php
@@ -1,7 +1,22 @@
+<head>
 <?php 
 	if(!isset($_SERVER['HTTPS'])) {
         $redir = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         header("Location: $redir");
     }
 ?>
+
+<style>
+table
+{
+border-collapse:collapse;
+}
+table, td, th
+{
+border:1px solid black;
+text-align:center;
+}
+</style>
+
+</head>
 <li><a href="<?php echo base_url();?>login/do_logout/" target="_blank">Logout</a></li>
