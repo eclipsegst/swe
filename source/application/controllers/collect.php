786ef423aa0f2a12399b48b146214caefc0afa4d
Zhaolong <zztg2@mail.missouri.edu>
2014-04-19 16:34:52 -0500

    Added download_single and download_all function

--- source/application/controllers/collect.php
+++ source/application/controllers/collect.php
@@ -13,6 +13,16 @@ class Collect extends CI_Controller {
 		$this->load->view('collect_view', array('error' => ' ' ));
 	}
 
+	function download_single()
+	{
+		$filepath = $this->input->get('filepath',true);
+		$filename = $this->input->get('filename',true);
+		$this->load->helper('download');
+		$data = file_get_contents($filepath);
+		$name = $filename;
+		force_download($name, $data);
+	}
+
 	function download_all()
 	{
 		$this->load->library('zip');
@@ -21,25 +31,6 @@ class Collect extends CI_Controller {
 	    $path = 'p/'. $courseid .'/'. $aname .'/';
 	    $this->zip->read_dir($path);  
 	    $result = $this->zip->download($courseid.'_'.$aname.'.zip'); 
-
-		// Download the file to your desktop. Name it "my_backup.zip"
-		// $this->zip->download('my_backup.zip');
-		// $this->zip->read_dir('./assets/', FALSE);
-
-		// $courseid = $this->input->get('courseid',true);
-		// $aname = $this->input->get('aname',true);
-		// $path = './p/'. $courseid . $aname .'/';
-		// // $this->zip->read_dir('assets/');
-		// $this->load->library('zip');
-		// $this->zip->read_dir('./p/CS 8001/Homework 1/');
-		// $this->index();
-
-		// $this->load->helper('download');
-		// $data = file_get_contents("./uploads/Inside_Llewyn_Davis_Poster.jpg"); // Read the file's contents
-		// $name = 'myphoto.jpg';
-		// force_download($name, $data);
-		// header("Content-Type: application/force-download");
-		// header("Content-Disposition: attachment; filename=\"./uploads/Inside_Llewyn_Davis_Poster.jpg\"");
 	}
 }
 
