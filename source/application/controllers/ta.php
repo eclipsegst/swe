f0df20479c9a269e7506ecdc7ccbdaeb670432c3
Zhaolong <zztg2@mail.missouri.edu>
2014-04-20 23:59:25 -0500

    Modified professor and ta page, which will only allow authorized professor and ta to access the two page.

--- source/application/controllers/ta.php
+++ source/application/controllers/ta.php
@@ -5,12 +5,13 @@ class Ta extends CI_Controller {
 	public function index($msg = NULL)
 	{
 		$role = $this->session->userdata('role');
+		$pawprint = $this->session->userdata('pawprint');
 		if($role=='ta' || $role=='admin'){
 			$data['msg'] = $msg;
 		    $this->load->model('course_model');
-		    $query = $this->course_model->get_all_courses();
-		    
+		    $query = $this->course_model->get_course_by_ta_pawprint($pawprint);
 		    $data['query'] = $query->result();
+		    $data['pawprint'] = $pawprint;
 			$this->load->view('ta_view', $data);
 		}else{
 			redirect('login');
