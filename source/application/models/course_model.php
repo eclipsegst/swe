958a3124ed7e3aac3966b5898027302229e7ee1d
Zhaolong <zztg2@mail.missouri.edu>
2014-04-20 23:54:27 -0500

    Add several functions used by ta_new and professor_new page.

--- source/application/models/course_model.php
+++ source/application/models/course_model.php
@@ -37,5 +37,34 @@ class Course_model extends CI_Model{
     	$this->db->where('courseid',$courseid);
         $this->db->delete('Courses');
     }
+    function insertPro($data){
+        $this->db->insert('pro_course',$data);
+    }
+    function get_pro(){
+        $query = $this->db->get('pro_course');
+        return $query;
+    }
+    function selectByProPawprint($pawprint)
+    {
+        $query = $this->db->get_where('pro_course', array('pawprint' =>$pawprint));
+        return $query;
+    }
+    function insertTa($data){
+        $this->db->insert('ta_course',$data);
+    }
+    function get_ta(){
+        $query = $this->db->get('ta_course');
+        return $query;
+    }
+
+    function get_ta_by_courseid($courseid){
+        $query = $this->db->get_where('ta_course', array('courseid' =>$courseid));
+        return $query;
+    }
+    function get_course_by_ta_pawprint($pawprint)
+    {
+        $query = $this->db->get_where('ta_course', array('pawprint' =>$pawprint));
+        return $query;
+    }
 }
-?>
+    
