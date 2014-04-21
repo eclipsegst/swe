c8d8444612885e79e5e8c240eaa89f1807b78700
Zhaolong <zztg2@mail.missouri.edu>
2014-04-21 00:00:27 -0500

    Modified the layout of ta_view

--- source/application/views/ta_view.php
+++ source/application/views/ta_view.php
@@ -1,15 +1,17 @@
+<?php include './assets/template/header.php'; ?>
 <a href="<?php echo base_url();?>login/do_logout/">Logout</a>| Welcome <?php echo $this->session->userdata('firstname');?></a>
 <h1> Welcome to TA page </h1>
 
 <form action="<?php echo base_url();?>course_new"><input type="submit" value="Added a new course"></form>
 
 <div>
-	<table style="width:1200px">
+	<table style="width:300px">
 		<tr>
-		 	<th>cid</th>
+		 	
 		 	<th>courseid</th>
-		    <th>coursename</th>
-		    <th>description</th>    
+		 	<th>TA Pawprint</th>
+		 	<th>Action</th>
+  
 		    
 		</tr>
 		<?php 
@@ -17,13 +19,10 @@
 		{
 		?>
 		<tr>
-			<td><?php echo $course->cid; ?></td>
 			<td><?php echo $course->courseid; ?></td>
-      		<td><?php echo $course->coursename; ?></td>
-      		<td><?php echo $course->description; ?></td>
+			<td><?php echo $pawprint; ?></td>
 			<td>
-			<a href="<?php echo base_url(); ?>assignment_new?courseid=<?php echo $course->courseid; ?>">Add an assignment</a>
-			<a href="<?php echo base_url(); ?>course_single?courseid=<?php echo $course->courseid; ?>">Select</a>
+			<a href="<?php echo base_url(); ?>course_single?courseid=<?php echo $course->courseid; ?>">Select this course</a>
 			</td>
 		<tr>
 		<?php
