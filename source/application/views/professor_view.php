8f85abb8816e1edc4a835b86c73f567e0458c98b
Zhaolong <zztg2@mail.missouri.edu>
2014-04-20 23:56:43 -0500

    Modifed the layout and add a list of TAs. Adding a TA to a specified course works.

--- source/application/views/professor_view.php
+++ source/application/views/professor_view.php
@@ -1,2 +1,50 @@
 <a href="<?php echo base_url();?>login/do_logout/">Logout</a> | Welcome <?php echo $this->session->userdata('firstname');?>
 <h1> Welcome to Profeesor page </h1>
+
+<div>
+	<table style="width:300px">
+		<tr>
+		 	<th>Courseid</th>
+		    <th>Professor</th>      
+		    
+		</tr>
+		<?php 
+		foreach ($user as $course)
+		{
+			
+		?>
+		<tr>
+			<td><?php echo $course->courseid; ?></td>
+      		<td><?php echo $course->pawprint; ?></td>
+			<td>
+			<a href="<?php echo base_url(); ?>ta_new?courseid=<?php echo $course->courseid; ?>">Add a TA</a>
+			</td>
+		<tr>
+		<?php 
+		}
+		?>
+	</table>
+</div>
+
+<h3>List of TAs</h3>
+<div>
+	<table style="width:200px">
+		<tr>
+		 	<th>Courseid</th>
+		    <th>TA</th>      
+		    
+		</tr>
+		<?php 
+		foreach ($ta as $item)
+		{
+			
+		?>
+		<tr>
+			<td><?php echo $item->courseid; ?></td>
+      		<td><?php echo $item->pawprint; ?></td>
+		<tr>
+		<?php 
+		}
+		?>
+	</table>
+</div>
