86a128b13c14d31674b2405ae8cab7b999f47c9c
jmabp7 <jmabp7@mail.missouri.edu>
2014-04-22 01:47:02 -0500

    update to collection table with hierarchy

--- source/collectionTable/Collection.sql
+++ source/collectionTable/Collection.sql
@@ -1,6 +1,8 @@
 CREATE TABLE Collection (
-	id SERIAL, 
+	course  VARCHAR(40) not null,
+	assignment VARCHAR(40) not null,
+	section VARCHAR(40) not null, 
 	pawprint VARCHAR(40) not null, 
 	file VARCHAR(40) not null,
-	PRIMARY KEY (id)
+	PRIMARY KEY (pawprint)
 );
