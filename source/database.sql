a9759f937e49ea5519c003c4c7672fc39a2f2efd
Zhaolong <zztg2@mail.missouri.edu>
2014-04-23 21:48:03 -0500

    edit database.sql

--- source/database.sql
+++ source/database.sql
@@ -1,10 +1,12 @@
+DROP SCHEMA IF EXISTS project;
+
 CREATE SCHEMA project;
 
 SET search_path = project, public;
 
 DROP TABLE IF EXISTS Admin;
 CREATE TABLE Admin (
-	id int(11) SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	pawprint varchar(25) NOT NULL,
 	password varchar(20) NOT NULL
 );
@@ -13,7 +15,7 @@ INSERT INTO Admin VALUES (1,'ar442','5735297383'),(2,'jsf2pc','5732917407'),(3,'
 
 DROP TABLE IF EXISTS Assignments;
 CREATE TABLE Assignments (
-	aid int(10) SERIAL PRIMARY KEY,
+	aid SERIAL PRIMARY KEY,
 	courseid varchar(10) NOT NULL default '' REFRENCES Courses,
 	aname varchar(50) NOT NULL default '',
 	duedate varchar(50) NOT NULL default '',
@@ -24,7 +26,7 @@ INSERT INTO Assignments VALUES (1,'CS4320','Homework #1',NULL,NULL),(2,'CS4320',
 
 DROP TABLE IF EXISTS Courses;
 CREATE TABLE Courses (
-	cid int(10) SERIAL PRIMARY KEY,
+	cid SERIAL PRIMARY KEY,
 	courseid varchar(10) NOT NULL default '' REFERENCES Courses,
 	coursename varchar(50) NOT NULL default '',
 	description varchar(250) NOT NULL default ''
@@ -34,7 +36,7 @@ INSERT INTO Courses VALUES (1,'CS4320','Software Engineering I - SP2014','you ca
 
 DROP TABLE IF EXISTS Create_assignments;
 CREATE TABLE Create_assignments (
-	caid int(10) SERIAL PRIMARY KEY,
+	caid SERIAL PRIMARY KEY,
 	courseid varchar(10) NOT NULL default '' REFERENCES Courses,
 	aname varchar(50) NOT NULL default ''
 );
@@ -43,14 +45,14 @@ INSERT INTO Create_Assignments VALUES (1,'CS4320','Homework #1');
 
 DROP TABLE IF EXISTS Enroll;
 CREATE TABLE Enroll (
-	eid int(10) SERIAL PRIMARY KEY,
+	eid SERIAL PRIMARY KEY,
 	pawprint varchar(25) NOT NULL default '',
 	courseid varchar(10) NOT NULL REFERENCES Courses
 );
 
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
-	uid int(10) SERIAL PRIMARY KEY,
+	uid SERIAL PRIMARY KEY,
 	lastname varchar(20) NOT NULL DEFAULT '',
 	firstname varchar(20) NOT NULL DEFAULT '',
 	pawprint varchar(25) NOT NULL DEFAULT '',
@@ -62,7 +64,7 @@ INSERT INTO `Users` VALUES (14160001,'Rama Akula','Amit','ar442','5735297383',3)
 
 DROP TABLE IF EXISTS pro_ta_course;
 CREATE TABLE pro_ta_course (
-	ptc int(10) SERIAL PRIMARY KEY,
+	ptc SERIAL PRIMARY KEY,
 	courseid varchar(10) NOT NULL REFERENCES Courses,
 	ppawprint varchar(25) NOT NULL DEFAULT '',
 	tpawprint varchar(25) NOT NULL DEFAULT ''
@@ -70,6 +72,12 @@ CREATE TABLE pro_ta_course (
 
 INSERT INTO `pro_ta_course` VALUES (1,'CS4320',NULL,NULL),(2,'CS4380',NULL,NULL),(3,'CS4520',NULL,NULL);
 
-
+DROP TABLE IF EXISTS log;
+CREATE TABLE log (
+	log_id SERIAL PRIMARY KEY,
+	pawprint varchar(25) NOT NULL DEFAULT '',
+	log_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	action varchar(50) NOT NULL
+);
 
 
