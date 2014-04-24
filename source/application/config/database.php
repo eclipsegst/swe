1ceaf29f3f33cd9325e512659f441dee563939e7
Zhaolong <zztg2@mail.missouri.edu>
2014-04-24 14:58:41 -0500

    Changed db to mysql

--- source/application/config/database.php
+++ source/application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'dbhost-mysql.cs.missouri.edu';
 $db['default']['username'] = 'cs4320s14grp14';
-$db['default']['password'] = 'WtWHcx2m';
+$db['default']['password'] = 'Tvy8.vmPx=';
 $db['default']['database'] = 'cs4320s14grp14';
-$db['default']['dbdriver'] = 'postgre';
+$db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
@@ -64,6 +64,21 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
+// $db['default']['hostname'] = 'dbhost-mysql.cs.missouri.edu';
+// $db['default']['username'] = 'cs4320s14grp14';
+// $db['default']['password'] = 'WtWHcx2m';
+// $db['default']['database'] = 'cs4320s14grp14';
+// $db['default']['dbdriver'] = 'postgre';
+// $db['default']['dbprefix'] = '';
+// $db['default']['pconnect'] = TRUE;
+// $db['default']['db_debug'] = TRUE;
+// $db['default']['cache_on'] = FALSE;
+// $db['default']['cachedir'] = '';
+// $db['default']['char_set'] = 'utf8';
+// $db['default']['dbcollat'] = 'utf8_general_ci';
+// $db['default']['swap_pre'] = '';
+// $db['default']['autoinit'] = TRUE;
+// $db['default']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
