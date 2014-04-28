34f601ad930154a97c1b012c9e911cd90c4bb8e0
KPETG6 <kpetg6@mail.missouri.edu>
2014-04-28 16:28:31 -0500

    Integrated LDAP authentication

--- source/application/controllers/login.php
+++ source/application/controllers/login.php
@@ -5,11 +5,12 @@ class Login extends CI_Controller {
 	public function index($msg = NULL)
 	{
 		$data['msg'] = $msg;
-		$this->load->model('login_model');
+		// $this->load->model('login_model');
 		$this->load->model('auth');
-		$query = $this->login_model->get_all_users();
-		$data['query'] = $query->result();
-		$this->load->view('login_view', $data);
+		// $query = $this->login_model->get_all_users();
+		// $data['query'] = $query->result();
+		$this->load->view('login_view');
+		// $this->process();
 	}
 
 	function process()
