e685bf9ffa7dae56e363b1eb33bfa8a1bcb1223c
ar442 113816 <ar442@babbage.cs.missouri.edu>
2014-04-28 20:09:11 -0500

    updated acceptor

---
+++ source/application/controllers/acceptorc.php
@@ -0,0 +1,214 @@
+<?php
+//Filename/path = Course/section/assn/pawprint/filename-hash_timestamp.extension
+//$path='./uploads/' . $course . '/' . $section . '/' . $assn . '/'; + filename goes here
+class Acceptorc extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		
+	
+	}
+	
+	public function index() 
+	{
+	
+		//TODO: get the file from: ['upload_path'] = './uploads/';	
+		//Initialize error message to empty string
+		//echo 'here we are';
+		$errorMessage = "";
+		$course = $this->input->post('course');
+		$section = $this->input->post('section');
+		$assignment = $this->input->post('assignment');
+		$recieved_hash = $this->input->post('hash');
+		$user = $this->input->post('user');
+		$file_path = './p/' . $course . '/' . $assignment . '/' . $section . '/' .$user;
+		echo $file_path;
+		if (!is_dir($file_path))
+		{
+			mkdir($file_path, 0777, TRUE);
+		}
+		$config['upload_path'] = $file_path;
+		$config['allowed_types'] = '*';
+		$config['max_size']= '5242880';
+		$config['min_size']= '1';
+		
+		$this->load->library('upload', $config);
+
+		if($this->upload->do_upload())
+		{
+			echo "successfull";
+			//$data = array('upload_data' => $this->upload->data());
+			// $this->load->view('upload_success', $data);
+			//redirect('upload_success');
+		}
+		else
+		{
+			echo "Upload failed.";
+			//$this->index($msg);
+		}
+		
+		//to do: check valid params
+		
+		$filedetails = $this->upload->data();
+		$file_name = $filedetails['file_name'];
+		$isSameHash = $this->check_hash($recieved_hash, $file_path . '/' . $file_name);
+		
+		if($isSameHash != TRUE) 
+		{
+			echo 'hash = ' . $isSameHash;
+			$errorMessage .= "There was an error sending the file.\nExpected:" . $recieved_hash . "\nRecieved:" . $isSameHash . "\n\n";
+			//delete file as hashes dont match
+			unlink($file_path . '/' . $file_name);
+			return;
+		}
+		/************************
+		//make a log entry for the addi req
+		*******************************/
+		$query = 'insert into log (pawprint,courseid,aid,sectionid,action,hash,filename) 
+		values (' .$user .',' .$course. ',' .$assignment. ',' .$section.',File Upload,' .$recieved_hash.',' .$file_name.')' ;
+		echo $query;
+		$this->db->query($query);
+			
+		//$isSizeOkay = check_file_size($File);
+		//$isValidParams = check_params($course, $section, $assignment);
+		//$isSameHash = check_hash($recieved_hash), $file_path);
+		
+		//no need to check the file size since we do this on both front end interfaces
+			/* if($isSizeOkay == FALSE) {
+			
+			$errorMessage = $errorMessage . "The file must be less than 5MB\n";
+			} */
+			
+			
+			
+			/*if($isValidParams != 000) {
+			//Somehow figure out how to distinguish between the good vs bad params. Maybe use chmod protocol, eg 0-7
+			//Then use a switch to determine the error message
+			
+			//100 = course
+			//010 = section
+			//001 = assn
+				switch($isValidParams) {
+
+				case 001:	$errorMessage .= "Invalid assignment\n";
+					break;
+					
+				case 010:	$errorMessage .= "Invalid section\n";
+					break;
+					
+				case 011:	$errorMessage .= "Invalid section & assignment\n";
+					break;
+					
+				case 100:	$errorMessage .= "Invalid course\n";
+					break;
+					
+				case 101:	$errorMessage .= "Invalid course & assignment\n";
+					break;
+					
+				case 110:	$errorMessage .= "Invalid course & section\n";
+					break;
+					
+				case 111:	$errorMessage .= "Invalid course, section, & assignment\n";
+					break;
+				
+				
+				}
+			}
+			
+			//If everything looks good
+			if(empty($errorMessage)) {
+			
+			
+			//An error occured
+			} else {
+			$errorMessage = "\n========  ERROR ========\n\n" . $errorMessage;
+			//handle errors ->log and send message to user
+			}*/
+	
+	
+	}
+	
+	
+	//Return true if the file is within the bounds
+	//otherwise retiurn false
+	//@deprecated
+	
+/* 	function check_file_size($File) 
+	{
+	$size = filesize(File);
+	
+		if($size > 5242880) {
+		//error file too large
+		//break/ call error
+		return FALSE;
+		} else if($size == FALSE) {
+		//Could not calculate filesize
+		return FALSE;
+		}
+	return TRUE;
+	
+	 }*/
+	
+	////////////////////////////// Return TRUE if successful
+	///////////////////////////// Return FALSE if an error occured
+	// Return a 3 bit binary digit to tell which parameters were invalid
+	function check_params($course, $section, $assignment) {
+	$validity = 000;
+	
+	//Check course
+		if ($isValidCourse != TRUE) {
+		$validity = $validity + 100;
+		}
+		
+	//Check section
+		if($isValidSection != TRUE) {
+		$validity = $validity + 10;
+		}
+		
+	//Check Assignment
+		if($isValidAssn != TRUE) {
+		$validity = $validity + 1;
+		}
+	
+	//Check each argument against the data in the DB
+	//Throw any necessary errors
+	
+	return $validity;
+	}
+
+	// Compare the recieved_hash to the has that we
+	// generate on the file. 
+	// Case 1: If they do not match, the file was
+	// damaged, send error message back to user
+	// return new_hash
+	// Case 2: match, everything is good, 
+	// return TRUE
+	public function check_hash($received_hash, $file_path)
+	{
+		$hash = hash_file( "sha256", $file_path);
+		if($hash == $received_hash) 
+		{
+			return  TRUE;
+		} 
+		else 
+		{
+			return $hash;
+		}
+	
+	}
+	
+	function store_file($file_name, $course, $section, $assignment) {
+		$timestamp = time();
+		
+		// IF /course/assignment/section/$pawprint !exists, create the directory
+		
+		if(move_uploaded_file ($_FILES[file_up][tmp_name], $file_name.$timestamp)){
+			echo "Congratulations, file succesfully uploaded";
+		} else {
+			echo "Failed to upload file";
+			}
+	}
+
+
+}
+?>
---
+++ source/p/CS4320/Homework2/1/ar442/sub_header.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <curl/curl.h>
+#include <openssl/evp.h>
+
+#define EVP_MAX_MD_SIZE 64 
+
+#define MAX_FILE_SIZE 5242880	//5MB
+
+
+char *course;
+char *section;
+char *filename;
+char *assign;
+char *course_section;
+struct stat st;
+off_t fSize;
+char *userName;
+struct passwd *pwptr;
+char url[255];
+char webname[255];
+struct string {
+  char *ptr;
+  size_t len;
+};
+unsigned char md_value[EVP_MAX_MD_SIZE];
+void init_string(struct string *s);
+
+size_t writeFunc(void *ptr, size_t size, size_t nmemb, struct string *s);
+
+void cleanup();
+
+int validateParametersPassed();
+
+int validateFilePassed();
+
+//TO DO
+char *getPawprint();
+
+
+int sendFile();
+void GenerateHashCode();
